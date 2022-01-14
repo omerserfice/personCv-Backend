d8d1025f811f189b26c95adba5f7e45c712a6f76
omerserfice <srfcomr@gmail.com>
2022-01-15 00:39:49 +0300

    proje oluşturuldu.

---
+++ personApp.DAL/Configuration/PersonConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using personApp.DAL.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personApp.DAL.Configuration
+{
+    public class PersonConfiguration : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.HasKey(p => p.PersonId);
+            builder.Property(p => p.PersonId).ValueGeneratedOnAdd();
+            builder.Property(p => p.PersonName).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.PersonSurname).HasMaxLength(150).IsRequired();
+            builder.Property(p=>p.PersonCity).IsRequired();
+            builder.Property(p=>p.PersonBirthDay).IsRequired();
+
+        }
+    }
+}
---
+++ personApp.DAL/Entites/Person.cs
@@ -0,0 +1,20 @@
+﻿using personAppCore.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personApp.DAL.Entites
+{
+    public class Person : Audit , IEntity, ISoftDeleted
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public string PersonSurname { get; set; }
+        public DateTime PersonBirthDay { get; set; }
+        public string PersonCity { get; set; }
+        public ICollection<Ability> Abilities { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
